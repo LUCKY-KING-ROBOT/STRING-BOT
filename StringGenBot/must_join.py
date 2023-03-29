61ff8c48a7347415b2c50a030ecf75a1ecca62bc
LUCKY-KING-ROBOT <127356753+LUCKY-KING-ROBOT@users.noreply.github.com>
2023-03-29 09:19:05 +0530

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -18,9 +18,9 @@ async def must_join_channel(bot: Client, msg: Message):
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
-                await msg.reply_photo(photo="https://telegra.ph/file/bb54da6e1586fd1cde94f.jpg", caption=f"» ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [『 💫 ꜱᴇᴅ ʙᴏy ꜱᴛʀɪɴɢꜱᴇꜱꜱɪᴏɴ 💫 』]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [『 💫 ꜱᴇᴅ ʙᴏy ꜱᴛʀɪɴɢꜱᴇꜱꜱɪᴏɴ 💫 』]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
+                await msg.reply_photo(photo="https://telegra.ph/file/bb54da6e1586fd1cde94f.jpg", caption=f"» ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [WORLD FRIEND]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [WORLD FRIEND]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("『 💫 ꜱᴇᴅ ʙᴏy ꜱᴛʀɪɴɢꜱᴇꜱꜱɪᴏɴ 💫 』", url=f"{link}")]
+                        [InlineKeyboardButton("WORLD FRIEND", url=f"{link}")]
                     ])
                 )
                 await msg.stop_propagation()
