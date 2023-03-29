4a7977ff01bc4f5a9697b24cef43103f82554fbb
LUCKY-KING-ROBOT <127356753+LUCKY-KING-ROBOT@users.noreply.github.com>
2023-03-29 09:12:00 +0530

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -18,7 +18,7 @@ async def must_join_channel(bot: Client, msg: Message):
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
-                await msg.reply_photo(photo="https://telegra.ph/file/bb54da6e1586fd1cde94f.jpg", caption=f"» ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [ꜱᴇᴅʙᴏy ꜱᴛʀɪɴɢꜱ]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [ꜱᴇᴅʙᴏy ꜱᴛʀɪɴɢꜱ]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
+                await msg.reply_photo(photo="https://telegra.ph/file/bb54da6e1586fd1cde94f.jpg", caption=f"» ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [『 💫 ꜱᴇᴅ ʙᴏy ꜱᴛʀɪɴɢꜱᴇꜱꜱɪᴏɴ 💫 』]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [『 💫 ꜱᴇᴅ ʙᴏy ꜱᴛʀɪɴɢꜱᴇꜱꜱɪᴏɴ 💫 』]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("『 💫 ꜱᴇᴅ ʙᴏy ꜱᴛʀɪɴɢꜱᴇꜱꜱɪᴏɴ 💫 』", url=f"{link}")]
                     ])
